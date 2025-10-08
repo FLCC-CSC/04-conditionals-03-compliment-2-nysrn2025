fb95b3560d378cb359eeef80741ee6ed9b51748d
nysrn2025 <klee24@fingerlakes.edu>
2025-10-08 04:26:42 +0000

    Add compliment_02.py with custom yes/no messages

--- compliment_02.py
+++ compliment_02.py
@@ -1,6 +1,91 @@
-git add compliment_02.py
-git commit -m "Recreate compliment_02.py to fix path issues"
-git push origin main
+# FILE NAME - compliment_02.py
+
+# NAME: KEVIN LEE
+# DATE: 10/8/2025
+# BRIEF DESCRIPTION: This program asks the user if they want a compliment and responds
+#  with custom messages for exact 'yes' vs any other input 
 
 
 
+# 1. Make sure you fill out the comments above
+# 2. Write your code in the proper spot
+# 3. Be sure to answer the Reflection Questions and Attestation below
+# 4. The Sample Output has been included in this code for your convenience
+
+
+
+########## ENTER YER CODE BELOW THIS LINE ##########
+# Ask user if they want a compliment
+user_input = input("Would you like a compliment? ")
+
+# Check if input is exactly "yes" (lowercase)
+if user_input == 'yes':
+    print("You have wonderful eyes.")
+    print("Thank you for playing.")
+else:
+    # For any other input, show custom message
+    print("No compliment for you!")
+    print("Thank you for playing.")
+
+
+
+
+
+
+
+
+
+########### END YER CODE ABOVE THIS LINE ###########
+
+    
+
+
+
+########################################
+#          SAMPLE OUTPUT
+########################################
+
+'''
+Would you like a compliment? yes
+You have wonderful eyes.
+Thank you for playing.
+'''
+
+
+'''
+Would you like a compliment? Yes
+No compliment for you!
+Thank you for playing.
+'''
+
+
+'''
+Would you like a compliment? y
+No compliment for you!
+Thank you for playing.
+'''
+
+
+'''
+Would you like a compliment? no
+No compliment for you!
+Thank you for playing.
+'''
+
+
+
+########################################
+#          REFLECTION QUESTIONS
+########################################
+
+'''
+
+1. Did you struggle with this lab (YES/NO)?
+
+
+
+
+
+
+
+'''
